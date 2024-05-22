2d931d3e441c987e62280a4863e8389aef5e21a8
barbozaawill <williandeliz03@gmail.com>
2024-05-22 18:54:20 -0300

    atualização do exercício 1001

--- Beecrowd1001.java
+++ Beecrowd1001.java
@@ -1,21 +1,5 @@
-import java.util.Scanner;
-
 public class Beecrowd1001 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Digite sua idade: ");
-        int idade = sc.nextInt();
-
-        if (idade < 16) {
-            System.out.println("Você não tem idade para votar!");
-        }
-        sc.close();
-
+        System.out.println("Hello World");
     }
-    
-
-
-
-
 }
